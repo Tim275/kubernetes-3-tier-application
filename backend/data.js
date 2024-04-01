a5c2b3f3a2458d05d33aa1f8e8c1be31c8daf07e
Tim275 <sagichdirnicht259@outlook.de>
2024-04-01 05:10:02 +0200

    first commit

---
+++ backend/data.js
@@ -0,0 +1,64 @@
+module.exports = {
+  data: [
+    {
+      question: "What is the primary role of the Kubernetes Master?",
+      option1: "To schedule pods",
+      option2: "To manage the container runtime",
+      option3: "To handle the storage of persistent data",
+      option4: "To run application code",
+      ans: 1,
+    },
+    {
+      question: "What is a Kubernetes Pod?",
+      option1: "A container runtime",
+      option2: "The smallest and simplest unit in the Kubernetes object model",
+      option3: "A physical machine in the cluster",
+      option4: "A load balancer",
+      ans: 2,
+    },
+    {
+      question: "What is the purpose of a Kubernetes Service?",
+      option1: "To manage the container runtime",
+      option2: "To provide a way to expose an application running on a set of Pods as a network service",
+      option3: "To schedule pods",
+      option4: "To run application code",
+      ans: 2,
+    },
+    {
+      question: "What is a Kubernetes Deployment?",
+      option1: "A physical machine in the cluster",
+      option2: "A load balancer",
+      option3: "A way to manage updates to Pods and ReplicaSets",
+      option4: "A container runtime",
+      ans: 3,
+    },
+    {
+      question: "What is the role of a Kubernetes Node?",
+      option1: "To run application code",
+      option2: "To manage the container runtime",
+      option3: "A worker machine in Kubernetes",
+      option4: "To schedule pods",
+      ans: 3,
+    },
+
+
+
+    // {
+    //   question: "test?",
+    //   option1: "test",
+    //   option2: "test",
+    //   option3: "test",
+    //   option4: "test",
+    //   ans: 1,
+    // },
+    // {
+    //   question: "Who interpreted dreams in Egypt?",
+    //   option1: "ttt",
+    //   option2: "aaa",
+    //   option3: "sss",
+    //   option4: "Pharaoh",
+    //   ans: 1,
+    // },
+    // Add comments to deactivate additional questions
+  ],
+};
